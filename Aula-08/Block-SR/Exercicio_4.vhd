ddf6a40f54cc9cb4f6bc28f6a89dcf0f18097773
Marcos Rocha <37712508+dev-marcos@users.noreply.github.com>
2023-04-05 16:26:46 -0300

    Update Exercicio_4.vhd

--- Aula-08/Block-SR/Exercicio_4.vhd
+++ Aula-08/Block-SR/Exercicio_4.vhd
@@ -1,19 +1,16 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 ENTITY Exercicio_4 IS
-	PORT (	 A: IN std_logic;
-				 B: IN std_logic; 
-				EN: IN std_logic;
-			  SUM: OUT std_logic;
-			 COUT: OUT std_logic;
+	PORT (	 A, B, T, En: IN std_logic;
+		SUM, Cout: OUT std_logic;
 
 END Exercicio_4;
 
 ARCHITECTURE logica OF Exercicio_4 IS
-SIGNAL 
-
 BEGIN
-	SUM : 
-	
-	
+	bloco1: BLOCK(En='1')
+	BEGIN
+	      SUM <= GUARDED (A XOR B) WHEN T='1' ELSE 'Z';
+	     Cout <= GUARDED (A AND B) WHEN T='1' ELSE 'Z';
+	END BLOCK bloco1;
 END logica;
